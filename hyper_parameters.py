b8635b0daec211fc2a4e898f1cd7bd7cb5594786
Arindam Ghosh <arindam.gm@gmail.com>
2020-02-17 00:08:13 -0500

    Added code to extract 80 dim MFCC from TIMIT full dataset

---
+++ hyper_parameters.py
@@ -0,0 +1,79 @@
+import tensorflow as tf
+
+
+hparams_wavenet = tf.contrib.training.HParams(
+
+    # Training:
+    adam_beta1=0.9,
+    adam_beta2=0.999,
+    adam_eps=1e-8,
+    amsgrad=False,
+    initial_learning_rate=1e-3,
+    # see lrschedule.py for available lr_schedule
+    lr_schedule="noam_learning_rate_decay",
+    lr_schedule_kwargs={},  # {"anneal_rate": 0.5, "anneal_interval": 50000},
+    nepochs=2000,
+    weight_decay=0.0,
+    clip_thresh=-1,
+    # max time steps can either be specified as sec or steps
+    # if both are None, then full audio samples are used in a batch
+    max_time_sec=None,
+    max_time_steps=8000,
+    # Hold moving averaged parameters and use them for evaluation
+    exponential_moving_average=True,
+    # averaged = decay * averaged + (1 - decay) * x
+    ema_decay=0.9999,
+
+)
+
+# Default hyperparameters:
+hparams = tf.contrib.training.HParams(
+    # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
+    # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
+    cleaners='english_cleaners',
+    use_cmudict=False,  # Use CMUDict during training to learn pronunciation of ARPAbet phonemes
+
+    # Audio:
+    num_mels=80,
+    num_freq=1025,
+    sample_rate=16000,
+    frame_length_ms=50,
+    frame_shift_ms=12.5,
+    preemphasis=0.97,
+    min_level_db=-100,
+    ref_level_db=20,
+
+    # Model:
+    # TODO: add more configurable hparams
+    outputs_per_step=5,
+    padding_idx=None,
+    use_memory_mask=False,
+
+    # Data loader
+    pin_memory=True,
+    num_workers=2,
+
+    # Training:
+    batch_size=16,
+    adam_beta1=0.9,
+    adam_beta2=0.999,
+    initial_learning_rate=0.002,
+    decay_learning_rate=True,
+    nepochs=10000,
+    weight_decay=0.0,
+    clip_thresh=1.0,
+
+    # Save
+    checkpoint_interval=5000,
+
+    # Eval:
+    max_iters=200,
+    griffin_lim_iters=60,
+    power=1.5,              # Power to raise magnitudes to prior to Griffin-Lim
+)
+
+
+def hparams_debug_string():
+    values = hparams.values()
+    hp = ['  %s: %s' % (name, values[name]) for name in sorted(values)]
+    return 'Hyperparameters:\n' + '\n'.join(hp)
--- mfcc_extraction.py
+++ mfcc_extraction.py
@@ -1,12 +1,34 @@
 import numpy as np
 import torch
 from torch.utils import data
-# for accesing files in the directory
+# For accessing files in the directory
 import glob
 import errno
 
+import os, sys
+import audio
+import re
 
+# Training data
 train_audio_path = "./timit_data/data/TRAIN/*/*/*.WAV.wav"  #"./timit_data/data/TRAIN/*/*/*.TXT"
 train_audio_filenames = glob.glob(train_audio_path)
 train_text_filenames = [filename.replace("WAV.wav", "TXT") for filename in train_audio_filenames]
 
+# Test data
+test_audio_path = "./timit_data/data/TEST/*/*/*.WAV.wav"
+test_audio_filenames = glob.glob(test_audio_path)
+test_text_filenames = [filename.replace("WAV.wav", "TXT") for filename in test_audio_filenames]
+
+
+mfcc_dir_train = "./timit_data/mfcc_data/train"
+for wav_fname in train_audio_filenames:
+	fullname = wav_fname.split('/')[-1]
+	print(fullname)
+	fname = fullname.split('.')[0]
+	print(fname)
+	break
+	# wav_fname = wav_dir + '/' + fname + '.wav'
+	wav = audio.load_wav(wav_fname)
+	mel_spectrogram = audio.melspectrogram(wav).astype(np.float32)
+	mspec_fname = mfcc_dir_train + '/' + fname + '.feats'
+	np.save(mspec_fname, mel_spectrogram.T, allow_pickle=False)
